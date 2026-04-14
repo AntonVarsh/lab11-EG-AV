87ed2f4e82c9d1a5dda153b7c8d1a29c113b3efe
avarshavsky <varshavskyanton@gmail.com>
2026-04-13 20:11:02 -0400

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -17,17 +17,17 @@ class TestCalculator(unittest.TestCase):
     # def test_multiply(self): # 3 assertions
     #     fill in code
     def test_multiply(self):
-        self.assertEqual(calculator.multiply(2, 3), 6)
+        self.assertEqual(mul(2, 3), 6)
 
     # def test_divide(self): # 3 assertions
     #     fill in code
     def test_divide(self):
-        self.assertEqual(calculator.divide(2, 10), 5)
+        self.assertEqual(div(2, 10), 5)
 
     ######## Partner 2
     def test_divide_by_zero(self):  # 1 assertion
         with self.assertRaises(ZeroDivisionError):
-            divide(0, 5)
+            div(0, 5)
 
     def test_logarithm(self):  # 3 assertions
         self.assertEqual(logarithm(10, 100), 2)
@@ -46,12 +46,12 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            calculator.logarithm(1, 0)
+            logarithm(1, 0)
 
     # def test_hypotenuse(self): # 3 assertions
     #     fill in code
     def test_hypotenuse(self):
-        self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
 
     # def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
@@ -60,7 +60,7 @@ class TestCalculator(unittest.TestCase):
     #     # Test basic function
     #     fill in code
     def test_sqrt(self):
-        self.assertAlmostEqual(calculator.square_root(16), 4)
+        self.assertAlmostEqual(square_root(16), 4)
 
 
 # Do not touch this
